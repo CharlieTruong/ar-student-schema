8a900e930864696bca13647bf58ffd265fb679a6
dbc-apprentice <apprentice@devbootcamp.com>
2014-01-22 14:26:39 -0600

    defined students scehma

--- db/migrate/20121011144238_create_students.rb
+++ db/migrate/20121011144238_create_students.rb
@@ -1,9 +1,22 @@
 require_relative '../config'
 
-# this is where you should use an ActiveRecord migration to 
+# this is where you should use an ActiveRecord migration to
 
 class CreateStudents < ActiveRecord::Migration
   def change
-    # HINT: checkout ActiveRecord::Migration.create_table
+    # # HINT: checkout ActiveRecord::Migration.create_table
+    create_table :students do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :gender
+      t.date   :birthday
+      t.string :email
+      t.string :phone
+      t.timestamps
+    end
   end
+
+  # def down
+  #   drop_table :products
+  # end
 end
